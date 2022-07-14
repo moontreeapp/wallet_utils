1974850ebaf4a3cdbfa1da1a824080cec3ad8a61
Jordan Miller <Jordan.kay@gmail.com>
2022-07-14 17:55:15 -0600

    updated derivation api

--- lib/src/utilities/derivation.dart
+++ lib/src/utilities/derivation.dart
@@ -4,17 +4,22 @@ class Derivation {
   static const externalNumber = 0;
   static const internalNumber = 1;
 
-  static String getPath(
-    int index, {
+  /// returns a full derivation path if the index is supplied
+  static String getPath({
+    int? index,
+    int? chain,
+    int? account,
+    int? network,
+    int? exposure,
     bool mainnet = true,
     bool external = true,
   }) =>
       "m/"
-      "$ravencoinNumber'/"
-      "${mainnetNumber(mainnet)}'/"
-      "$accountNumber'/"
-      "${receiveNumber(external)}/"
-      "$index";
+      "${chain ?? ravencoinNumber}'/"
+      "${network ?? mainnetNumber(mainnet)}'/"
+      "${account ?? accountNumber}'/"
+      "${exposure ?? receiveNumber(external)}"
+      "${index != null ? '/$index' : ''}";
 
   static int receiveNumber([bool external = true]) =>
       external ? externalNumber : internalNumber;
