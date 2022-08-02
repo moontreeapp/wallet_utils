2089e219eb2248caa6779c0cbee485aa0177050f
Jordan Miller <Jordan.kay@gmail.com>
2022-08-02 13:40:26 -0600

    improved api on darive utility

--- lib/src/utilities/derive.dart
+++ lib/src/utilities/derive.dart
@@ -2,14 +2,18 @@ import 'package:ravencoin_wallet/ravencoin_wallet.dart'
     show HDWallet, mainnet, testnet;
 
 class Derive {
-  static HDWallet getHDWallet(String pubkey, {bool useMainet = false}) =>
+  static HDWallet walletFromPubkey(String pubkey, {bool useMainet = false}) =>
       HDWallet.fromBase58(pubkey, network: useMainet ? mainnet : testnet);
 
-  static HDWallet deriveHDWallet(
-    String pubkey,
-    String path,
-    int index, {
+  static HDWallet subWallet({
+    String? pubkey,
+    HDWallet? hdWallet,
+    required String path,
+    required int index,
     bool useMainet = false,
   }) =>
-      getHDWallet(pubkey, useMainet: useMainet).derivePath('$path/$index');
+      hdWallet != null
+          ? hdWallet.derivePath('$path/$index')
+          : walletFromPubkey(pubkey!, useMainet: useMainet)
+              .derivePath('$path/$index');
 }
