8a73407428d5db97b4e9cddcb63f5ee65ab0ecdd
Jordan Miller <Jordan.kay@gmail.com>
2022-12-06 22:04:44 -0700

    explored parseSendAmountAndFeeFromSerializedTransaction

--- lib/src/utils/transaction_fee_validation.dart
+++ lib/src/utils/transaction_fee_validation.dart
@@ -17,63 +17,65 @@ Tuple2<Map<String?, int>, int> parseSendAmountAndFeeFromSerializedTransaction(
     serializedTransaction = serializedTransaction.sublist(2);
   }
 
-  final vinCount = varuint.decode(serializedTransaction);
+  final int vinCount = varuint.decode(serializedTransaction);
   serializedTransaction =
       serializedTransaction.sublist(varuint.encodingLength(vinCount));
 
-  final vins = <Input>[];
-  for (var vinIdx = 0; vinIdx < vinCount; vinIdx++) {
-    final hash = serializedTransaction.sublist(0, 32);
+  final List<Input> vins = <Input>[];
+  for (int vinIdx = 0; vinIdx < vinCount; vinIdx++) {
+    final Uint8List hash = serializedTransaction.sublist(0, 32);
     serializedTransaction = serializedTransaction.sublist(32);
-    final idxBytes = serializedTransaction.sublist(0, 4);
+    final Uint8List idxBytes = serializedTransaction.sublist(0, 4);
     serializedTransaction = serializedTransaction.sublist(4);
-    final idx = idxBytes.buffer.asByteData().getUint32(0, Endian.little);
-    final scriptLength = varuint.decode(serializedTransaction);
+    final int idx = idxBytes.buffer.asByteData().getUint32(0, Endian.little);
+    final int scriptLength = varuint.decode(serializedTransaction);
     serializedTransaction =
         serializedTransaction.sublist(varuint.encodingLength(scriptLength));
-    final scriptBytes = serializedTransaction.sublist(0, scriptLength);
+    final Uint8List scriptBytes =
+        serializedTransaction.sublist(0, scriptLength);
     serializedTransaction = serializedTransaction.sublist(scriptLength);
-    final sequenceBytes = serializedTransaction.sublist(0, 4);
+    final Uint8List sequenceBytes = serializedTransaction.sublist(0, 4);
     serializedTransaction = serializedTransaction.sublist(4);
-    final sequence =
+    final int sequence =
         sequenceBytes.buffer.asByteData().getUint32(0, Endian.little);
     vins.add(
         Input(hash: hash, index: idx, script: scriptBytes, sequence: sequence));
   }
 
-  final voutCount = varuint.decode(serializedTransaction);
+  final int voutCount = varuint.decode(serializedTransaction);
   serializedTransaction =
       serializedTransaction.sublist(varuint.encodingLength(voutCount));
 
-  final voutValues = <String?, int>{};
-  for (var voutIdx = 0; voutIdx < voutCount; voutIdx++) {
-    final satsBytes = serializedTransaction.sublist(0, 8);
+  final Map<String?, int> voutValues = <String?, int>{};
+  for (int voutIdx = 0; voutIdx < voutCount; voutIdx++) {
+    final Uint8List satsBytes = serializedTransaction.sublist(0, 8);
     serializedTransaction = serializedTransaction.sublist(8);
-    final sats = satsBytes.buffer.asByteData().getUint64(0, Endian.little);
-    final scriptLength = varuint.decode(serializedTransaction);
+    final int sats = satsBytes.buffer.asByteData().getUint64(0, Endian.little);
+    final int scriptLength = varuint.decode(serializedTransaction);
     serializedTransaction =
         serializedTransaction.sublist(varuint.encodingLength(scriptLength));
     Uint8List scriptBytes = serializedTransaction.sublist(0, scriptLength);
     serializedTransaction = serializedTransaction.sublist(scriptLength);
 
-    final asset_start = scriptBytes.indexOf(0xc0);
+    final int assetStart = scriptBytes.indexOf(0xc0);
     // Where it will normally be at minimum asset txs
-    if (asset_start > 0x16) {
-      scriptBytes = scriptBytes.sublist(asset_start + 1);
-      final push = pushdata.decode(scriptBytes, 0);
+    if (assetStart > 0x16) {
+      scriptBytes = scriptBytes.sublist(assetStart + 1);
+      final pushdata.DecodedPushData? push = pushdata.decode(scriptBytes, 0);
       scriptBytes = scriptBytes.sublist(push!.size! + 3);
-      final scriptType = scriptBytes[0];
+      final int scriptType = scriptBytes[0];
       scriptBytes = scriptBytes.sublist(1);
-      final nameLength = scriptBytes[0];
+      final int nameLength = scriptBytes[0];
       scriptBytes = scriptBytes.sublist(1);
-      final nameBytes = scriptBytes.sublist(0, nameLength);
+      final Uint8List nameBytes = scriptBytes.sublist(0, nameLength);
       scriptBytes = scriptBytes.sublist(nameLength);
-      final assetName = utf8.decode(nameBytes);
+      final String assetName = utf8.decode(nameBytes);
       if (scriptType == 0x6f) {
         voutValues[assetName] = 100000000;
       } else {
-        final satsBytes = scriptBytes.sublist(0, 8);
-        final sats = satsBytes.buffer.asByteData().getUint64(0, Endian.little);
+        final Uint8List satsBytes = scriptBytes.sublist(0, 8);
+        final int sats =
+            satsBytes.buffer.asByteData().getUint64(0, Endian.little);
         voutValues[assetName] = (voutValues[assetName] ?? 0) + sats;
       }
     } else {
@@ -82,14 +84,15 @@ Tuple2<Map<String?, int>, int> parseSendAmountAndFeeFromSerializedTransaction(
   }
   // Don't care about rest.
 
-  final vinValues = <String?, int>{};
-  for (final vin in vins) {
-    final amountTuple = outputsForVins[
+  final Map<String?, int> vinValues = <String?, int>{};
+  for (final Input vin in vins) {
+    final Tuple2<String?, int> amountTuple = outputsForVins[
         '${hex.encode(vin.hash!.reversed.toList())}:${vin.index!}']!;
     vinValues[amountTuple.item1] =
         (vinValues[amountTuple.item1] ?? 0) + amountTuple.item2;
   }
 
   // (output amounts, fee)
-  return Tuple2(voutValues, vinValues[null]! - voutValues[null]!);
+  return Tuple2<Map<String?, int>, int>(
+      voutValues, vinValues[null]! - voutValues[null]!);
 }
